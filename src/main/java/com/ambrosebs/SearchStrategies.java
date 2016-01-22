d03d28f23020fc8cc2b8601ace946bbff04a2fac
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-01-22 14:04:24 -0500

    travis

---
+++ src/main/java/com/ambrosebs/SearchStrategies.java
@@ -0,0 +1,5 @@
+package com.ambrosebs;
+
+public class SearchStrategies {
+
+}
---
+++ src/test/java/com/ambrosebs/SearchTest.java
@@ -0,0 +1,11 @@
+package com.ambrosebs;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SearchTest {
+  @Test
+  public void isGood() {
+    assertEquals(1, 1+1);
+  }
+}
